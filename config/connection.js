d749ab1995318d510893df295bea26eacf2d8a46
Vittorioemot <105027649+Vittorioemot@users.noreply.github.com>
2022-08-23 14:38:08 +0800

    changed mysql server address

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
+        host: '110.175.171.250',
         dialect: 'mysql',
         port: 3306
     });
